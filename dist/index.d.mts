7188740457228c69ff754473e8bf0adcda1bd1f3
asafarim <asafarim.it@gmail.com>
2024-06-29 22:44:55 +0200

    last build added

--- dist/index.d.mts
+++ dist/index.d.mts
@@ -3,9 +3,14 @@ import React from 'react';
 interface NavLink {
     label: string;
     href: string;
+    subNav?: NavLink[];
 }
 interface NavLinksProps {
     links: NavLink[];
+    className?: string;
+    style?: React.CSSProperties;
+    baseLinkStyle?: React.CSSProperties;
+    subLinkStyle?: React.CSSProperties;
 }
 declare const NavLinks: React.FC<NavLinksProps>;
 
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,105 @@
 "use strict";
+function _array_like_to_array(arr, len) {
+    if (len == null || len > arr.length) len = arr.length;
+    for(var i = 0, arr2 = new Array(len); i < len; i++)arr2[i] = arr[i];
+    return arr2;
+}
+function _array_with_holes(arr) {
+    if (Array.isArray(arr)) return arr;
+}
+function _define_property(obj, key, value) {
+    if (key in obj) {
+        Object.defineProperty(obj, key, {
+            value: value,
+            enumerable: true,
+            configurable: true,
+            writable: true
+        });
+    } else {
+        obj[key] = value;
+    }
+    return obj;
+}
+function _iterable_to_array_limit(arr, i) {
+    var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"];
+    if (_i == null) return;
+    var _arr = [];
+    var _n = true;
+    var _d = false;
+    var _s, _e;
+    try {
+        for(_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true){
+            _arr.push(_s.value);
+            if (i && _arr.length === i) break;
+        }
+    } catch (err) {
+        _d = true;
+        _e = err;
+    } finally{
+        try {
+            if (!_n && _i["return"] != null) _i["return"]();
+        } finally{
+            if (_d) throw _e;
+        }
+    }
+    return _arr;
+}
+function _non_iterable_rest() {
+    throw new TypeError("Invalid attempt to destructure non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+function _object_spread(target) {
+    for(var i = 1; i < arguments.length; i++){
+        var source = arguments[i] != null ? arguments[i] : {};
+        var ownKeys = Object.keys(source);
+        if (typeof Object.getOwnPropertySymbols === "function") {
+            ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function(sym) {
+                return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+            }));
+        }
+        ownKeys.forEach(function(key) {
+            _define_property(target, key, source[key]);
+        });
+    }
+    return target;
+}
+function ownKeys(object, enumerableOnly) {
+    var keys = Object.keys(object);
+    if (Object.getOwnPropertySymbols) {
+        var symbols = Object.getOwnPropertySymbols(object);
+        if (enumerableOnly) {
+            symbols = symbols.filter(function(sym) {
+                return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+            });
+        }
+        keys.push.apply(keys, symbols);
+    }
+    return keys;
+}
+function _object_spread_props(target, source) {
+    source = source != null ? source : {};
+    if (Object.getOwnPropertyDescriptors) {
+        Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+    } else {
+        ownKeys(Object(source)).forEach(function(key) {
+            Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+        });
+    }
+    return target;
+}
+function _sliced_to_array(arr, i) {
+    return _array_with_holes(arr) || _iterable_to_array_limit(arr, i) || _unsupported_iterable_to_array(arr, i) || _non_iterable_rest();
+}
 function _type_of(obj) {
     "@swc/helpers - typeof";
     return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj;
+}
+function _unsupported_iterable_to_array(o, minLen) {
+    if (!o) return;
+    if (typeof o === "string") return _array_like_to_array(o, minLen);
+    var n = Object.prototype.toString.call(o).slice(8, -1);
+    if (n === "Object" && o.constructor) n = o.constructor.name;
+    if (n === "Map" || n === "Set") return Array.from(n);
+    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _array_like_to_array(o, minLen);
 }
 var __create = Object.create;
 var __defProp = Object.defineProperty;
@@ -71,14 +169,53 @@ __export(src_exports, {
 module.exports = __toCommonJS(src_exports);
 // src/NavLinks.tsx
 var import_react = __toESM(require("react"));
+// src/NavbarLinks.module.css
+var NavbarLinks_default = {};
+// src/NavLinks.tsx
 var NavLinks = function(param) {
-    var links = param.links;
-    return /* @__PURE__ */ import_react.default.createElement("nav", null, /* @__PURE__ */ import_react.default.createElement("ul", null, links.map(function(link, index) {
+    var links = param.links, className = param.className, style = param.style, baseLinkStyle = param.baseLinkStyle, subLinkStyle = param.subLinkStyle;
+    var _ref = _sliced_to_array((0, import_react.useState)(null), 2), openDropdown = _ref[0], setOpenDropdown = _ref[1];
+    var handleMouseEnter = function(index) {
+        setOpenDropdown(index);
+    };
+    var handleMouseLeave = function() {
+        setOpenDropdown(null);
+    };
+    return /* @__PURE__ */ import_react.default.createElement("nav", {
+        className: className !== null && className !== void 0 ? className : NavbarLinks_default.navContainer,
+        style: _object_spread({}, style)
+    }, /* @__PURE__ */ import_react.default.createElement("ul", {
+        className: NavbarLinks_default.baseLinks,
+        style: _object_spread({}, baseLinkStyle)
+    }, links.map(function(link, index) {
         return /* @__PURE__ */ import_react.default.createElement("li", {
-            key: index
+            key: index,
+            onMouseEnter: function() {
+                return handleMouseEnter(index);
+            },
+            onMouseLeave: handleMouseLeave,
+            style: {
+                position: "relative"
+            }
         }, /* @__PURE__ */ import_react.default.createElement("a", {
             href: link.href
-        }, link.label));
+        }, link.label), link.subNav && link.subNav.length > 0 && openDropdown === index && /* @__PURE__ */ import_react.default.createElement("ul", {
+            style: _object_spread_props(_object_spread({}, subLinkStyle), {
+                position: "absolute",
+                top: "100%",
+                left: 0,
+                backgroundColor: "white",
+                border: "1px solid #ccc",
+                padding: "10px",
+                zIndex: 100
+            })
+        }, link.subNav.map(function(subLink, subIndex) {
+            return /* @__PURE__ */ import_react.default.createElement("li", {
+                key: subIndex
+            }, /* @__PURE__ */ import_react.default.createElement("a", {
+                href: subLink.href
+            }, subLink.label));
+        })));
     })));
 };
 var NavLinks_default = NavLinks;
