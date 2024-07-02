9579b5fead4876302d60532dcbf8fad75c8d35be
asafarim <asafarim.it@gmail.com>
2024-07-03 00:30:25 +0200

    Add TypeScript Support

---
+++ dist/NavLinks.js
@@ -0,0 +1,92 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importStar(require("react"));
+var NavbarLinks_module_css_1 = __importDefault(require("./NavbarLinks.module.css"));
+var NavLinks = function (_a) {
+    var links = _a.links, className = _a.className, baseLinkStyle = _a.baseLinkStyle, subLinkStyle = _a.subLinkStyle, _b = _a.isRightAligned, isRightAligned = _b === void 0 ? false : _b, _c = _a.isBottomAligned, isBottomAligned = _c === void 0 ? false : _c;
+    var _d = (0, react_1.useState)({}), openDropdown = _d[0], setOpenDropdown = _d[1];
+    var handleToggle = function (key) {
+        setOpenDropdown(function (prev) {
+            var _a;
+            return (__assign(__assign({}, prev), (_a = {}, _a[key] = !prev[key], _a)));
+        });
+    };
+    var renderSubNav = function (subNav, parentIndex) {
+        if (!subNav)
+            return null;
+        return (react_1.default.createElement("ul", { className: className, style: __assign(__assign({}, subLinkStyle), { position: "absolute" }) }, subNav.map(function (subLink, subIndex) {
+            var key = "".concat(parentIndex, "-").concat(subIndex);
+            return (react_1.default.createElement("li", { key: key, style: { position: "relative" }, className: isRightAligned ? NavbarLinks_module_css_1.default.rightAligned : isBottomAligned ? NavbarLinks_module_css_1.default.bottomAligned : undefined },
+                react_1.default.createElement("a", { href: subLink.href, onClick: function (e) {
+                        if (subLink.subNav) {
+                            e.preventDefault();
+                            handleToggle(key);
+                        }
+                    } },
+                    subLink.emoji && react_1.default.createElement("span", null, subLink.emoji),
+                    " ",
+                    subLink.iconLeft && react_1.default.createElement("i", { className: subLink.iconLeft }),
+                    " ",
+                    subLink.label,
+                    "  ",
+                    subLink.iconRight && react_1.default.createElement("i", { className: subLink.iconRight })),
+                openDropdown[key] && renderSubNav(subLink.subNav, key)));
+        })));
+    };
+    return (react_1.default.createElement("ul", { className: "".concat(NavbarLinks_module_css_1.default.baseLinks, " ").concat(className), style: baseLinkStyle }, links.map(function (link, index) {
+        var key = index.toString();
+        return (react_1.default.createElement("li", { key: key, className: isRightAligned ? NavbarLinks_module_css_1.default.rightAligned : isBottomAligned ? NavbarLinks_module_css_1.default.bottomAligned : undefined, style: { position: "relative" } },
+            react_1.default.createElement("a", { href: link.href, onClick: function (e) {
+                    if (link.subNav) {
+                        e.preventDefault();
+                        handleToggle(key);
+                    }
+                } },
+                link.emoji && react_1.default.createElement("span", null, link.emoji),
+                " ",
+                link.iconLeft && react_1.default.createElement("i", { className: link.iconLeft }),
+                " ",
+                link.label,
+                "  ",
+                link.iconRight && react_1.default.createElement("i", { className: link.iconRight })),
+            openDropdown[key] && renderSubNav(link.subNav, key)));
+    })));
+};
+exports.default = NavLinks;
